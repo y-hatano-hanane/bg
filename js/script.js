2bf2cc3b8eff18ab15f162cee7fd33d7cb0aeb1d
y-hatano-hanane <hatano@hanane.co.jp>
2020-09-11 16:06:44 +0900

    土台完成※ヒーローを除く

---
+++ js/script.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded', (e) => {
+
+  let faq_triger = document.querySelectorAll('.faq_q');
+  let faq_active;
+  const className_faq_active = 'open';
+
+  let window_h = window.innerHeight;
+  const fixedFooter = document.getElementById('fixedFooter');
+
+  window.addEventListener( 'resize', function() {
+    window_h = window.innerHeight;
+  }, false );
+  window.addEventListener('load', set_fixedFooter);
+  window.addEventListener('scroll', set_fixedFooter);
+
+
+  faq_triger.forEach(function(el, i) {
+    el.addEventListener('click', function() {
+      if ( !el.classList.contains(className_faq_active)) {
+        if ( faq_active ) {
+          faq_active.classList.remove(className_faq_active);
+        }
+        faq_active = el;
+        el.classList.add(className_faq_active)
+      } else {
+        el.classList.remove(className_faq_active)
+      }
+    })
+  })
+
+  function set_fixedFooter() {
+    if (document.body.scrollTop > window_h/2 ) {
+      fixedFooter.classList.add('active')
+    } else {
+      fixedFooter.classList.remove('active')
+    }
+  }
+
+});
+
