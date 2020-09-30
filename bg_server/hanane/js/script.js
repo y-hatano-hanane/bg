868cd604cc79b9d1d83db6cb330def01880d2447
y-hatano-hanane <hatano@hanane.co.jp>
2020-09-30 13:31:31 +0900

    JS IE対応（FAQ除く）

--- bg_server/hanane/js/script.js
+++ bg_server/hanane/js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', (e) => {
+window.addEventListener('DOMContentLoaded', function(e) {
 
   let faq_triger = document.querySelectorAll('.faq_q');
   let faq_active;
@@ -13,9 +13,10 @@ window.addEventListener('DOMContentLoaded', (e) => {
   window.addEventListener('load', set_fixedFooter);
   window.addEventListener('scroll', set_fixedFooter);
 
-
-  faq_triger.forEach(function(el, i) {
-    el.addEventListener('click', function() {
+  for (let i = 0; i<faq_triger.length; i++) {
+    faq_triger[i].addEventListener('click', function() {
+      let el = faq_triger[i];
+      console.log(el.classList.contains(className_faq_active))
       if ( !el.classList.contains(className_faq_active)) {
         if ( faq_active ) {
           faq_active.classList.remove(className_faq_active);
@@ -26,7 +27,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
         el.classList.remove(className_faq_active)
       }
     })
-  })
+  }
 
   function set_fixedFooter() {
     if (window.pageYOffset > window_h/2 ) {
